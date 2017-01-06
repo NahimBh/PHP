53835f32aa60fd029ba0f42f62f23b0ab4d3376d
nahim <test@test.fr>
2017-01-06 11:25:56 +0100

    Ajout du site de la CVSAE

---
+++ site_CVSAE/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>CVSAE</title>
+        
+    <link rel="stylesheet" href="css/style.css" />  
+    </head>
+    
+<body> 
+<header>
+    <nav>
+        <ul>
+            <li><a href="#accueil">Accueil</a></li>
+            <li><a href="#">Actualités</a></li>
+            <li><a href="#reservation">Naviguer</a></li>
+            <li><a href="#contacts">Contacts</a></li>
+            <li><a href="#connection" class="bouton1">Connexion</a></li>
+            <li><a href="#inscription">S'inscrire</a></li>
+        </ul>
+    </nav>
+    
+    <h1 id="#accueil">Base de Bédanne</h1>
+    <a><p id="explorer">Explorer</p></a>
+</header>
+    
+<main>
+<p></p>    
+</main>
+</body>
+</html>
